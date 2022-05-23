31818f60a9b44f3726bd2295524259d368c52649
zulux99 <83884087+zulux99@users.noreply.github.com>
2022-05-23 11:11:47 +0200

    działające routy

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,39 @@
 import { createRoot } from "react-dom/client";
 import Navbar from "./Navbar";
-import Przekierowania from "./Przekierowania";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import Przekierowania from "./Przekierowania";
 
 function App() {
   return (
     <>
       <div>
-        {/* <Navbar /> */}
-        <Przekierowania />
-        {/* Hello world! */}
+        <HashRouter>
+          <Routes>
+            {/* <Navbar /> */}
+            {/* <Przekierowania /> */}
+            <Route path="/" element={<Bills />} />
+            {/* Hello world! */}
+            <Route path="bills" element={<Bills />} />
+          </Routes>
+        </HashRouter>
       </div>
     </>
   );
 }
-
+function Bills() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the bills!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/bills">About</Link>
+      </nav>
+    </>
+  );
+}
 export default App;
 
 const container = document.getElementById("index");
