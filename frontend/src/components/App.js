716f9e2661383ffaa371db2e867075c255689540
zulux99 <szymon14059@gmail.com>
2022-05-25 17:45:55 +0200

    axios get lista użytkowników

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,42 @@
 import { createRoot } from "react-dom/client";
-import { Link, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Bills from "./Bills";
 import HomePage from "./HomePage";
 import Navbar from "./Navbar";
 
 function App() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const getUser = () => {
+      axios
+        .get("/api/user")
+        .then((res) => {
+          console.log(res.data);
+          setUser(res.data);
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    };
+    getUser();
+  }, []);
+  let userData = user.map((val) => {
+    return (
+      <>
+        <p>{val.username}</p>
+        <p>{val.id}</p>
+      </>
+    );
+  });
   return (
     <>
       <BrowserRouter>
         <div>
           <Navbar />
-          <Link to="/">HomePage</Link>
-          <br />
-          <Link to="bills">Bils</Link>
         </div>
+        <div>{userData}</div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="bills" element={<Bills />} />
@@ -24,7 +46,6 @@ function App() {
   );
 }
 export default App;
-
 const container = document.getElementById("index");
 const root = createRoot(container);
 root.render(<App />);
