7e81c0a0d025240c11bcb69b377609a24e7bf5a1
zulux99 <szymon14059@gmail.com>
2022-08-29 23:30:38 +0200

    zmienianie rachunku

--- frontend/src/components/Bills.js
+++ frontend/src/components/Bills.js
@@ -2,21 +2,26 @@ import AuthContext from "../context/AuthContext";
 import { useState } from "react";
 import axios from "axios";
 import { useContext } from "react";
-import { Button, IconButton, TextField } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import { Button, IconButton, InputLabel, List, ListItem, TextField } from "@mui/material";
+import { Box, Container, width } from "@mui/system";
 import { useEffect } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Check from "@mui/icons-material/Check";
 
 function Bills() {
   const user = useContext(AuthContext);
   const [nazwa, setNazwa] = useState("");
   const [bills, setBills] = useState([]);
   const [billAlreadyExists, setBillAlreadyExists] = useState(false);
+  const [billsEditingNow, setBillsEditingNow] = useState([]);
   const user_id = user.user.user_id;
 
   useEffect(() => {
     showBills();
-    return () => {};
+    return () => {
+      setBills([]);
+    };
   }, []);
   useEffect(() => {
     bills.find((bill) => bill.nazwa === nazwa) ? setBillAlreadyExists(true) : setBillAlreadyExists(false);
@@ -35,6 +40,26 @@ function Bills() {
       console.log(bills);
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const changeBillName = async (bill) => {
+    try {
+      const response = await axios.put(
+        `/api/bills/update/${bill.id}`,
+        JSON.stringify({ user: user_id, nazwa: bill.nazwa }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.authTokens.access}`,
+            id: user_id,
+          },
+        }
+      );
+      console.log(response.data);
+      showBills();
+    } catch (err) {
+      console.log(err.response.data);
     }
   };
 
@@ -57,39 +82,62 @@ function Bills() {
     e.preventDefault();
     if (!billAlreadyExists) {
       try {
-        const response = await axios.post("/api/bills", JSON.stringify({ user: user.user.user_id, nazwa }), {
+        const response = await axios.post("/api/bills", JSON.stringify({ user: user_id, nazwa }), {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${user.authTokens.access}`,
           },
         });
-        showBills();
         e.target.reset();
         setNazwa("");
+        showBills();
       } catch (err) {
         console.log(err.response.data);
       }
     }
   };
-
   return (
     <>
       <Container align="center">
-        {bills.map((bill, index) => (
-          <Box key={index}>
-            <p>
-              {bill.nazwa}
-              <IconButton onClick={() => deleteBill(bill.id)}>
-                <DeleteIcon />
-              </IconButton>
-            </p>
-          </Box>
-        ))}
+        <List>
+          {bills.map((bill, index) => (
+            <Box key={index}>
+              <ListItem key={index}>
+                <TextField
+                  defaultValue={bill.nazwa}
+                  variant="standard"
+                  sx={{ display: billsEditingNow.find((id) => id === index + 1) ? "block" : "none" }}
+                  onChange={(e) => ((bills[index].nazwa = e.target.value), console.log(bills))}
+                />
+                <InputLabel
+                  sx={{
+                    display: billsEditingNow.find((id) => id === index + 1) ? "none" : "block",
+                    border: "1 px solid black",
+                  }}>
+                  {bill.nazwa}
+                </InputLabel>
+                <Box sx={{ display: billsEditingNow.find((id) => id === index + 1) ? "none" : "block" }}>
+                  <IconButton onClick={() => setBillsEditingNow(billsEditingNow.concat(index + 1))}>
+                    <EditIcon color="info" />
+                  </IconButton>
+                </Box>
+                <Box sx={{ display: billsEditingNow.find((id) => id === index + 1) ? "block" : "none" }}>
+                  <IconButton onClick={() => changeBillName(bill)}>
+                    <Check color="success" />
+                  </IconButton>
+                </Box>
+                <IconButton onClick={() => deleteBill(bill.id)}>
+                  <DeleteIcon color="error" />
+                </IconButton>
+              </ListItem>
+            </Box>
+          ))}
+        </List>
         <form onSubmit={handleSubmit}>
           <TextField
             type="text"
             variant="outlined"
-            label="Nazwa rachunku"
+            label="Nowa nazwa rachunku"
             error={billAlreadyExists ? true : false}
             helperText={billAlreadyExists ? "Rachunek o tej nazwie już istnieje" : ""}
             fullWidth
