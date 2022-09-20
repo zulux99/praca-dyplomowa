032b08c66010d8765842b4120e991f2c2ae0b67e
zulux99 <83884087+zulux99@users.noreply.github.com>
2022-09-20 14:44:41 +0200

    .

--- projekt/settings.py
+++ projekt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend') 
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
