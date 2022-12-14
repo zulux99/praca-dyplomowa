d627f185a26ccfc438ad14b0e38bf2d0b4f4815f
zulux99 <szymon14059@gmail.com>
2022-12-14 18:53:59 +0100

    kategorie - wyświetlanie podział

--- frontend/src/components/categories/CategoryList.js
+++ frontend/src/components/categories/CategoryList.js
@@ -3,7 +3,19 @@ import Box from "@mui/material/Box";
 function CategoryList(props) {
   return (
     <Box className="kategorie_lista">
+      <h2>Kategorie przychodów</h2>
       {props.categoryList
+        .filter((category) => category.przychod === true)
+        .sort((a, b) => a.nazwa.localeCompare(b.nazwa))
+        .map((category) => (
+          <Box key={category.id} className="kategorie_lista_element">
+            {category.nazwa}
+            <button onClick={() => props.handleDelete(category.id)}>Usuń</button>
+          </Box>
+        ))}
+      <h2>Kategorie wydatków</h2>
+      {props.categoryList
+        .filter((category) => category.przychod === false)
         .sort((a, b) => a.nazwa.localeCompare(b.nazwa))
         .map((category) => (
           <Box key={category.id} className="kategorie_lista_element">
